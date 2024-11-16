e8c2a46c17948991d8a661f89c0b38c7d74f3156
Da-bin, Lee <0713vin@gmail.com>
2024-11-16 14:37:17 +0900

    eidt :zap: storage 생성 & upload 기능 구현

--- components/file-dragdropzone.tsx
+++ components/file-dragdropzone.tsx
@@ -1,17 +1,35 @@
 "use client";
 
 import { Button } from "@material-tailwind/react";
+import { useMutation } from "@tanstack/react-query";
+import { uploadFile } from "actions/storageActions";
+import { queryClient } from "config/ReactQueryClientProvider";
 import { useRef } from "react";
 
 export default function FileDragDropZone() {
   // 선행 되어야하는 것 👇
   // supabase Policies 설정 > 권한 설정 및 파일 설정 ⚙️
   const fileRef = useRef(null);
+  const uploadImageMutation = useMutation({
+    mutationFn: uploadFile,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["images"],
+      });
+    },
+  });
+
   return (
     <form
       onSubmit={(e) => {
         e.preventDefault();
         const file = fileRef.current.files?.[0];
+        if (file) {
+          const formData = new FormData();
+          formData.append("file", file);
+          const result = await uploadImageMutation.mutate(formData);
+          console.log(result);
+        }
       }}
       className="w-full py-20 border-4 border-dotted border-indigo-700 flex flex-col items-center justify-center"
     >
